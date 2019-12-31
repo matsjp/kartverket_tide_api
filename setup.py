b8e31a62d5dc6928a978325d2c81debce00c43c2
Mats Johan Pedersen <MatsPedersen@protonmail.com>
2019-12-31 17:29:43 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ setup(
   author = 'Mats Johan Pedersen',
   author_email = 'matspedersen@protonmail.com',
   url = 'https://github.com/matsjp/kartverket_tide_api',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['kartverket'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  download_url = 'https://github.com/matsjp/kartverket_tide_api/archive/v0.1-alpha.tar.gz',
+  keywords = ['kartverket'],
+  install_requires=[
           'requests',
           'vcrpy',
       ],
@@ -18,7 +18,7 @@ setup(
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.6',
   ],
 )
