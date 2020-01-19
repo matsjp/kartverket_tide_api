68da73104b0a5b0aa79c085d05c46e62807fad50
Mats Johan Pedersen <MatsPedersen@protonmail.com>
2020-01-19 21:20:47 +0100

    Update setup file

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'kartverket_tide_api',
   packages = ['kartverket_tide_api'],
-  version = '0.1',
+  version = '0.11',
   license='MIT',
   description = 'This is a unofficial library for the kartverket tide api',
   author = 'Mats Johan Pedersen',
   author_email = 'matspedersen@protonmail.com',
   url = 'https://github.com/matsjp/kartverket_tide_api',
-  download_url = 'https://github.com/matsjp/kartverket_tide_api/archive/v0.1-alpha.tar.gz',
+  download_url = 'https://github.com/matsjp/kartverket_tide_api/archive/v0.11-alpha.tar.gz',
   keywords = ['kartverket'],
   install_requires=[
           'requests',
