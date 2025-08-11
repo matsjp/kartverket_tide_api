89ba0a9deb3cfe2cf78e53b711e8cae417cab012
Mats Johan Pedersen <matspedersen@protonmail.com>
2025-08-11 19:40:13 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -1,25 +1,30 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
-  name = 'kartverket_tide_api',
-  packages = ['kartverket_tide_api', 'kartverket_tide_api/tideobjects', 'kartverket_tide_api/tests',
-              'kartverket_tide_api/parsers', 'kartverket_tide_api/exceptions'],
-  version = '0.13',
-  license='MIT',
-  description = 'This is a unofficial library for the kartverket tide api',
-  author = 'Mats Johan Pedersen',
-  author_email = 'matspedersen@protonmail.com',
-  url = 'https://github.com/matsjp/kartverket_tide_api',
-  download_url = 'https://github.com/matsjp/kartverket_tide_api/archive/v0.13-alpha.tar.gz',
-  keywords = ['kartverket'],
-  install_requires=[
-          'requests',
-          'vcrpy',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.6',
-  ],
+    name='kartverket_tide_api',
+    version='1.0.0',
+    license='MIT',
+    description='Unofficial library for the Kartverket tide API',
+    author='Mats Johan Pedersen',
+    author_email='matspedersen@protonmail.com',
+    url='https://github.com/matsjp/kartverket_tide_api',
+    download_url='https://github.com/matsjp/kartverket_tide_api/archive/refs/tags/v1.0.0.tar.gz',
+    keywords=['kartverket', 'tide', 'api'],
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'requests',
+        'vcrpy',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.6',
 )
